0457ac7dff4d47f520ff990a6d9c969c329b83cc
Aaron Waterhouse <aaronwaterhouse@gmail.com>
2014-08-13 22:29:12 -0700

    added unit tests

--- src/MainTests.java
+++ src/MainTests.java
@@ -1,3 +1,22 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
 public class MainTests {
 
+	@Test
+	public void testGarageCreation(){
+		Garage testGarage = new Garage(5);
+		assertTrue(testGarage.getGarageSize() == 5);
+	}
+	
+	@Test
+	public void testOldestCarInGarage(){
+		Garage testGarage = new Garage(5);
+		for(int i=0; i<5; i++){
+			Car car = new Car(2000 + (int)(Math.random() * ((2000 - 2000) + 1)), "test");
+			testGarage.addCarToGarage(car, i);
+		}
+		assertTrue(2000 == testGarage.getOldestCar().getYear());
+	}
 }
